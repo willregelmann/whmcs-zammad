ce01c499cc821cf461fe8d8f4cecd5de5cc33af1
Will Regelmann <will@regelmann.net>
2021-07-06 21:01:06 -0400

    Style cleanup

--- zammad/zammad.php
+++ zammad/zammad.php
@@ -1,30 +1,30 @@
 <?php
 
-function zammad_config(){
+function zammad_config():array {
     return [
-	"name" => "Zammad",
-        "description" => "Integrates with Zammad ticketing",
-        "version" => "0.1",
-        "author" => "Watch Communications",
-        "fields" => [
-            "url" => [
-                "FriendlyName" => "URL",
-                "Type" => "text",
-                "Size" => "255",
-                "Description" => "Zammad API URL (https://*/api/v1)"
-            ],
-            "token" => [
-                "FriendlyName" => "Token",
-                "Type" => "text",
-                "Size" => "255",
-                "Description" => "API token"
-            ],
-            "group" => [
-                "FriendlyName" => "Group",
-                "Type" => "text",
-                "Size" => "255",
-                "Description" => "Zammad group name"
-            ]
-        ]    
-    ];
+        "name" => "Zammad",
+            "description" => "Integrates with Zammad ticketing",
+            "version" => "0.1",
+            "author" => "Watch Communications",
+            "fields" => [
+                "url" => [
+                    "FriendlyName" => "URL",
+                    "Type" => "text",
+                    "Size" => "255",
+                    "Description" => "Zammad API URL (https://*/api/v1)"
+                ],
+                "token" => [
+                    "FriendlyName" => "Token",
+                    "Type" => "text",
+                    "Size" => "255",
+                    "Description" => "API token"
+                ],
+                "group" => [
+                    "FriendlyName" => "Group",
+                    "Type" => "text",
+                    "Size" => "255",
+                    "Description" => "Zammad group name"
+                ]
+            ]    
+        ];
 }
