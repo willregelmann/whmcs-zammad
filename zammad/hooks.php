af5f492bb76b6d67a506fd58fa6bb94cfd0ea967
Will Regelmann <will@regelmann.net>
2021-07-06 21:00:05 -0400

    Style cleanup

--- zammad/hooks.php
+++ zammad/hooks.php
@@ -1,8 +1,8 @@
 <?php
 
-function zammad_getconfig(){
-    $result = select_query( 'tbladdonmodules', '*', array( 'module' => 'zammad' ) );
-    $return = array();
+function zammad_getconfig():array {
+    $result = select_query('tbladdonmodules', '*', ['module' => 'zammad']);
+    $return = [];
     while ($data = mysql_fetch_array($result)) {
         if ($data['setting'] == 'url') {
             $return['url'] = $data['value'];
@@ -15,56 +15,55 @@ function zammad_getconfig(){
     return $return;
 }
 
-add_hook('TicketOpen', 1, function($vars){
+add_hook('TicketOpen', 1, function(array $args):void {
 
     $config = zammad_getconfig();
-    $customer = localAPI('GetClientsDetails', ['clientid'=>$vars['userid']], 'whmsystem');
+    $customer = localAPI('GetClientsDetails', ['clientid'=>$args['userid']], 'whmsystem');
 
     $params = [
-        "title" => $vars['subject'],
-        "group" => 'Server Team',
-        "article" => [
-            "content_type" => "text/html",
-            "body" => $vars['message'],
-            "internal" => false,
-            "sender_id" => 2,
-            "type" => "email",
-            "from" => $customer['email'],
-            "reply_to" => $customer['email']
+        'title' => $args['subject'],
+        'group' => 'Server Team',
+        'article' => [
+            'content_type' => 'text/html',
+            'body' => $args['message'],
+            'internal' => false,
+            'sender_id' => 2,
+            'type' => 'email',
+            'from' => $customer['email'],
+            'reply_to' => $customer['email']
         ],
-        "customer_id" => sprintf("guess:%s", $customer['email']),
-        "whmcs" => $vars['ticketid']
+        'customer_id' => "guess:$customer[email]",
+        'whmcs' => $args['ticketid']
     ];
 
     $zammad = curl_init();
-    $curl_opts = [
+    curl_setopt_array($zammad, [
         CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_URL => sprintf("%s/tickets",$config['url']),
+        CURLOPT_URL => "$config[url]/tickets",
         CURLOPT_HTTPHEADER => [
             'Content-type: application/json',
-            sprintf('Authorization: Token %s', $config['token'])
+            "Authorization: Token $config[token]"
         ],
         CURLOPT_SSL_VERIFYPEER => false,
-        CURLOPT_POST => 1,
+        CURLOPT_POST => true,
         CURLOPT_POSTFIELDS => json_encode($params)
-    ];
-    curl_setopt_array($zammad, $curl_opts);  
+    ]);  
     curl_exec($zammad);
 
 });
 
-add_hook('TicketUserReply', 1, function($vars){
+add_hook('TicketUserReply', 1, function(array $args):void {
 
     $config = zammad_getconfig();
-    $customer = localAPI('GetClientsDetails', ['clientid'=>$vars['userid']], 'whmsystem');
+    $customer = localAPI('GetClientsDetails', ['clientid'=>$args['userid']], 'whmsystem');
 
     $zammad = curl_init();
     $curl_opts = [
         CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_URL => sprintf("%s/tickets/search?query=whmcs:%s",$config['url'],$vars['ticketid']),
+        CURLOPT_URL => "$config[url]/tickets/search?query=whmcs:$args[ticketid]",
         CURLOPT_HTTPHEADER => [
             'Content-type: application/json',
-            sprintf('Authorization: Token %s', $config['token'])
+            "Authorization: Token $config[token]"
         ],
         CURLOPT_SSL_VERIFYPEER => false
     ];
@@ -73,67 +72,53 @@ add_hook('TicketUserReply', 1, function($vars){
     $ticket = array_values((array)$tickets->assets->Ticket)[0];
 
     $params = [
-        "ticket_id" => $ticket->id,
-        "content_type" => "text/html",
-        "body" => $vars['message'],
-        "origin_by_id" => $ticket->customer_id,
-        "from" => $customer['email'],
-        "reply_to" => $customer['email'],
-        "type" => "email",
-        "sender_id" => 2,
-        "internal" => false
+        'ticket_id' => $ticket->id,
+        'content_type' => 'text/html',
+        'body' => $args['message'],
+        'origin_by_id' => $ticket->customer_id,
+        'from' => $customer['email'],
+        'reply_to' => $customer['email'],
+        'type' => 'email',
+        'sender_id' => 2,
+        'internal' => false
     ];
 
-    $curl_opts = [
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_URL => sprintf("%s/ticket_articles", $config['url']),
-        CURLOPT_HTTPHEADER => [
-            'Content-type: application/json',
-            sprintf('Authorization: Token %s', $config['token'])
-        ],
-        CURLOPT_SSL_VERIFYPEER => false,
+    curl_setopt_array($zammad, array_replace($curl_opts, [
+        CURLOPT_URL => "$config[url]/ticket_articles",
         CURLOPT_POST => 1,
         CURLOPT_POSTFIELDS => json_encode($params)
-    ];
-    curl_setopt_array($zammad, $curl_opts);
+    ]));
     curl_exec($zammad);
 
 });
 
-add_hook('TicketClose', 1, function($vars){
+add_hook('TicketClose', 1, function(array $args):void {
 
     $config = zammad_getconfig();
 
     $zammad = curl_init();
     $curl_opts = [
         CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_URL => sprintf("%s/tickets/search?query=whmcs:%s",$config['url'],$vars['ticketid']),
+        CURLOPT_URL => "$config[url]/tickets/search?query=whmcs:$args[ticketid]",
         CURLOPT_HTTPHEADER => [
             'Content-type: application/json',
-            sprintf('Authorization: Token %s', $config['token'])
+            "Authorization: Token $config[token]"
         ],
         CURLOPT_SSL_VERIFYPEER => false
     ];
     curl_setopt_array($zammad, $curl_opts);
-    $tickets = json_decode(curl_exec($zammad),false);
+    
+    $tickets = json_decode(curl_exec($zammad), false);
     $ticket = array_values((array)$tickets->assets->Ticket)[0];
 
-    $params = [
-        "state" => "closed"
-    ];
-    $curl_opts = [
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_URL => sprintf("%s/tickets/%s", $config['url'], $ticket->id),
-        CURLOPT_HTTPHEADER => [
-            'Content-type: application/json',
-            sprintf('Authorization: Token %s', $config['token'])
-        ],
-        CURLOPT_SSL_VERIFYPEER => false,
+    curl_setopt_array($zammad, array_replace($curl_opts, [
+        CURLOPT_URL => "$config[url]/tickets/$ticket->id",
         CURLOPT_CUSTOMREQUEST => 'PUT',
-        CURLOPT_POST => 1,
-        CURLOPT_POSTFIELDS => json_encode($params)
-    ];
-    curl_setopt_array($zammad, $curl_opts);
+        CURLOPT_POST => true,
+        CURLOPT_POSTFIELDS => json_encode([
+                'state' => 'closed'
+            ])
+    ]));
     curl_exec($zammad);
 
 });
